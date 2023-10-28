726633a11b04eab85c518ce938f90df47e92767a
Mahmoud Zaytoni <mahmoudzaytoni2002@gmail.com>
2023-10-29 01:35:03 +0200

    Create Multiple environments settings

---
+++ settings/prod.py
@@ -0,0 +1,15 @@
+import os
+from .base import *
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
